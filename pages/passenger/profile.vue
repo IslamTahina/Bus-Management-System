e4654395c497c0a3e9950213ed138bce424a33e6
Islam <islamtahina2@gmail.com>
2025-03-31 12:52:15 -0300

    pages for user

---
+++ pages/passenger/profile.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PassengerProfile from '~/components/PassengerProfile.vue'
+</script>
+
+<template>
+  <div class="container mx-auto px-4 py-8">
+    <PassengerProfile />
+  </div>
+</template> 
--- pages/passenger/wallet.vue
+++ pages/passenger/wallet.vue
@@ -5,7 +5,6 @@ import WalletCard from '~/components/WalletCard.vue'
 <template>
   <div class="container mx-auto px-4 py-8 flex justify-center">
     <div class="max-w-2xl w-full">
-      <h1 class="text-2xl font-bold mb-6">My Wallet</h1>
       <WalletCard :show-purchase="true" />
     </div>
   </div>
